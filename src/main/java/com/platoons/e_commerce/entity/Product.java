a47105d8bd6318d9df0d7fe78c6dad65a8162d10
apoque96 <dereckcabrerang@gmail.com>
2025-08-16 17:21:00 -0600

    migrations for Product table

---
+++ src/main/java/com/platoons/e_commerce/entity/Product.java
@@ -0,0 +1,51 @@
+package com.platoons.e_commerce.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product extends BaseEntity{
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID productId;
+
+    @NotNull(message = "Name is required")
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    private double discount;
+
+    private double discountAmount;
+
+    private String measurements;
+
+    @NotNull(message = "Description is required")
+    @NotBlank(message = "Description is required")
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @NotNull(message = "Price is required")
+    @Size(min = 0, message = "Price must be greater than 0")
+    private double price;
+
+    @NotNull(message = "Stock quantity is required")
+    private int stockQuantity;
+}
